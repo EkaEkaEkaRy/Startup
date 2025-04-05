6376489381e0b5e8b5810d83bdb0df36bdaafa18
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2025-04-05 21:58:43 +0300

    add items detection

--- startup/lib/api/api.dart
+++ startup/lib/api/api.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:camera/camera.dart';
 //import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
@@ -9,7 +11,7 @@ class Apiservice {
       'https://musical-parakeet-7g7jx75jx2w6jx-8000.app.github.dev/';
 
   // получение всех пользователей
-  void getItems(XFile image) async {
+  Future<List> getItems(XFile image) async {
     try {
       // final response = await _dio.post('${backend_link}track', data: {image});
       var request =
@@ -27,13 +29,24 @@ class Apiservice {
       var response = await request.send();
 
       if (response.statusCode == 200) {
-        var responseBody = await response.stream.bytesToString();
-        print('Успешно загружено: $responseBody');
+        final responseBody = await response.stream.bytesToString();
+        final answer_json = jsonDecode(jsonDecode(responseBody));
+        return answer_json;
       } else {
         throw Exception('Error ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('$e');
+    }
+  }
+
+  Future<String> getPrice(String className) async {
+    final response =
+        await http.get(Uri.parse('${backend_link}price?class_name=$className'));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Failed to load price');
     }
   }
 }
---
+++ startup/lib/components/draw_output_image.dart
@@ -0,0 +1,102 @@
+import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+//import 'package:http/http.dart' as http;
+import 'package:startup/api/api.dart';
+import 'package:image/image.dart' as img;
+
+Future<XFile> drawOutputImage(XFile inputFile, List trackJson) async {
+  final bytes = await inputFile.readAsBytes();
+  final ui.Codec codec = await ui.instantiateImageCodec(bytes);
+  final ui.FrameInfo frame = await codec.getNextFrame();
+  final ui.Image image = frame.image;
+
+  final pictureRecorder = ui.PictureRecorder();
+  final canvas = Canvas(pictureRecorder);
+
+  File imageFile = File(inputFile.path);
+  img.Image? imageTemp = img.decodeImage(imageFile.readAsBytesSync());
+
+  // Масштабирование изображения
+  final scaleFactor = getScaleFactor(
+      image, Size(imageTemp!.width.toDouble(), imageTemp.height.toDouble()));
+
+  // Рисование исходного изображения
+  canvas.drawImage(
+    image,
+    Offset.zero,
+    Paint()..filterQuality = FilterQuality.high,
+  );
+
+  // Рисование результатов распознавания
+  for (var trackResult in trackJson) {
+    final box = trackResult['box'];
+    final name = trackResult['name'];
+    final confidence = trackResult['confidence'];
+
+    // Масштабирование координат
+    final x1 = box['x1'] * scaleFactor;
+    final y1 = box['y1'] * scaleFactor;
+    final x2 = box['x2'] * scaleFactor;
+    final y2 = box['y2'] * scaleFactor;
+
+    // Рисование прямоугольника
+    canvas.drawRect(
+      Rect.fromLTWH(x1, y1, x2 - x1, y2 - y1),
+      Paint()
+        ..color = Colors.blue
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 5,
+    );
+
+    // Рисование текста (название и вероятность)
+    drawText(
+        canvas, '${name} ${confidence.toStringAsFixed(2)}', Offset(x1, y1));
+
+    // Получение цены
+    final price = await Apiservice().getPrice(name);
+
+    // Рисование текста (цена)
+    drawText(canvas, 'Цена $price рублей', Offset(x1, y2));
+  }
+
+  // Получение изображения из canvas
+  final picture = pictureRecorder.endRecording();
+  final ui.Image drawnImage = await picture.toImage(image.width, image.height);
+
+  // Сохранение изображения в файл
+  final newBytes = await drawnImage.toByteData(format: ui.ImageByteFormat.png);
+  final Uint8List byteList = newBytes!.buffer.asUint8List();
+  final directory = await getApplicationDocumentsDirectory();
+  final outputFile = File('${directory.path}/output.png');
+  await outputFile.writeAsBytes(byteList);
+
+  return XFile(outputFile.path);
+}
+
+// Функция для рисования текста
+void drawText(Canvas canvas, String text, Offset offset) {
+  final textPainter = TextPainter(
+    text: TextSpan(
+      text: text,
+      style: TextStyle(
+        color: Colors.blue,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    textDirection: TextDirection.ltr,
+  )..layout();
+
+  textPainter.paint(canvas, offset);
+}
+
+// Масштабирование изображения
+double getScaleFactor(ui.Image image, Size canvasSize) {
+  return image.width > image.height
+      ? canvasSize.width / image.width
+      : canvasSize.height / image.height;
+}
