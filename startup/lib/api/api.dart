34f66f41d736bbc4a61860f3a460a057133cbc12
Ryabova2020 <76618274+EkaEkaEkaRy@users.noreply.github.com>
2025-04-05 20:10:53 +0300

    update api

--- startup/lib/api/api.dart
+++ startup/lib/api/api.dart
@@ -1,10 +1,10 @@
 import 'package:camera/camera.dart';
-import 'package:dio/dio.dart';
+//import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class Apiservice {
   // инициализация библиотек
-  final Dio _dio = Dio();
+  //final Dio _dio = Dio();
   final backend_link =
       'https://musical-parakeet-7g7jx75jx2w6jx-8000.app.github.dev/';
 
